411e5f04018fd6fc2316ea032431c75f96a75692
Hunter H <huntrar@gmail.com>
2021-03-23 22:48:26 -0700

    Retire support for Python 3.5 due to lxml 4.6.3 incompatibility

--- scrape/__init__.py
+++ scrape/__init__.py
@@ -1,4 +1,4 @@
 """scrape is a rule-based web crawler and information extraction tool capable of manipulating and merging new and existing documents. XML Path Language (XPath) and regular expressions are used to define rules for filtering content and web traversal. Output may be converted into text, csv, pdf, and/or HTML formats.
 """
 
-__version__ = "0.11.0"
+__version__ = "0.11.1"
